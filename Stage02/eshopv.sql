69ec67d307fd283a3355f5c50364d20892e9d1a9
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2018-12-10 13:45:32 +0700

    đổi tên file .sql thành eshopv.sql

--- Stage02/eshopv.sql
+++ Stage02/eshopv.sql
@@ -5,44 +5,43 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema QuanLySanPham
+-- Schema eshopv
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema QuanLySanPham
+-- Schema eshopv
 -- -----------------------------------------------------
-CREATE DATABASE IF NOT EXISTS `QuanLySanPham` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
-USE `QuanLySanPham` ;
+CREATE DATABASE IF NOT EXISTS `eshopv` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `eshopv` ;
 
 -- -----------------------------------------------------
--- Table `QuanLySanPham`.`TaiKhoan`
+-- Table `eshopv`.`TaiKhoan`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`TaiKhoan` (
-  `MaTaiKhoan` INT ZEROFILL NOT NULL,
+CREATE TABLE IF NOT EXISTS `eshopv`.`TaiKhoan` (
+  `MaTaiKhoan` INT ZEROFILL NOT NULL AUTO_INCREMENT,
   `TenDangNhap` VARCHAR(20) NOT NULL,
   `MatKhau` VARCHAR(20) NOT NULL,
   `TenHienThi` VARCHAR(40) NOT NULL,
   `DiaChi` VARCHAR(200) NULL,
   `DienThoai` VARCHAR(11) NULL,
   `Email` VARCHAR(40) NULL,
-  `BiXoa` SMALLINT NOT NULL,
+  `BiXoa` SMALLINT NOT NULL DEFAULT 1 COMMENT '1: chưa xóa, 0: bị xóa',
   `HinhDaiDienURL` VARCHAR(100) NULL,
   `MaLoaiTaiKhoan` SMALLINT ZEROFILL NOT NULL COMMENT '0: admin, 1: khach hang',
-  `BiXoa` SMALLINT NULL,
   PRIMARY KEY (`MaTaiKhoan`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QuanLySanPham`.`DonDatHang`
+-- Table `eshopv`.`DonDatHang`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`DonDatHang` (
+CREATE TABLE IF NOT EXISTS `eshopv`.`DonDatHang` (
   `MaDonDatHang` VARCHAR(11) NOT NULL,
   `MaTaiKhoan` INT ZEROFILL NOT NULL,
   `NgayLap` DATETIME NOT NULL,
   `NgayDuKienGiaoHang` DATETIME NOT NULL,
   `TongThanhTien` BIGINT NOT NULL,
-  `BiXoa` SMALLINT NULL,
+  `BiXoa` SMALLINT NOT NULL DEFAULT 1 COMMENT '1: chưa xóa, 0: bị xóa',
   `TinhTrang` SMALLINT NOT NULL COMMENT '0: chua giao, 1: da giao',
   `TenNguoiNhan` VARCHAR(40) NOT NULL,
   `SDTNguoiNhan` VARCHAR(11) NULL,
@@ -55,41 +54,41 @@ CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`DonDatHang` (
   INDEX `fk_MaTaiKhoan_MaTaiKhoan_idx` (`MaTaiKhoan` ASC), #VISIBLE,
   CONSTRAINT `fk_MaTaiKhoanDDH_MaTaiKhoanTK`
     FOREIGN KEY (`MaTaiKhoan`)
-    REFERENCES `QuanLySanPham`.`TaiKhoan` (`MaTaiKhoan`)
+    REFERENCES `eshopv`.`TaiKhoan` (`MaTaiKhoan`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QuanLySanPham`.`LoaiSanPham`
+-- Table `eshopv`.`LoaiSanPham`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`LoaiSanPham` (
+CREATE TABLE IF NOT EXISTS `eshopv`.`LoaiSanPham` (
   `MaLoaiSanPham` INT ZEROFILL NOT NULL,
   `TenLoaiSanPham` VARCHAR(100) NULL,
-  `BiXoa` SMALLINT NULL,
+  `BiXoa` SMALLINT NOT NULL DEFAULT 1 COMMENT '1: chưa xóa, 0: bị xóa',
   PRIMARY KEY (`MaLoaiSanPham`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QuanLySanPham`.`HangSanXuat`
+-- Table `eshopv`.`HangSanXuat`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`HangSanXuat` (
+CREATE TABLE IF NOT EXISTS `eshopv`.`HangSanXuat` (
   `MaHangSanXuat` INT ZEROFILL NOT NULL,
   `TenHangSanXuat` VARCHAR(100) NULL,
   `LogoURL` VARCHAR(100) NULL,
-  `BiXoa` SMALLINT NULL,
+  `BiXoa` SMALLINT NOT NULL DEFAULT 1 COMMENT '1: chưa xóa, 0: bị xóa',
   `HangSanXuatcol` VARCHAR(45) NULL,
   PRIMARY KEY (`MaHangSanXuat`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QuanLySanPham`.`SanPham`
+-- Table `eshopv`.`SanPham`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`SanPham` (
-  `MaSanPham` INT ZEROFILL NOT NULL,
+CREATE TABLE IF NOT EXISTS `eshopv`.`SanPham` (
+  `MaSanPham` INT ZEROFILL NOT NULL AUTO_INCREMENT,
   `TenSanPham` VARCHAR(100) NULL,
   `HinhURL` VARCHAR(100) NULL,
   `GiaSanPham` INT NULL,
@@ -99,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`SanPham` (
   `SoLuongBan` INT NULL,
   `SoLuotXem` INT NULL,
   `MoTa` TEXT NULL,
-  `BiXoa` SMALLINT NULL,
+  `BiXoa` SMALLINT NOT NULL DEFAULT 1 COMMENT '1: chưa xóa, 0: bị xóa',
   `MaLoaiSanPham` INT ZEROFILL NOT NULL,
   `MaHangSanXuat` INT ZEROFILL NOT NULL,
   PRIMARY KEY (`MaSanPham`),
@@ -107,21 +106,21 @@ CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`SanPham` (
   INDEX `fk_MaHangSanXuatSP_MaHangSanXuatHSX_idx` (`MaHangSanXuat` ASC), # VISIBLE,
   CONSTRAINT `fk_MaLoaiSanPhamSP_MaLoaiSanPhamLSP`
     FOREIGN KEY (`MaLoaiSanPham`)
-    REFERENCES `QuanLySanPham`.`LoaiSanPham` (`MaLoaiSanPham`)
+    REFERENCES `eshopv`.`LoaiSanPham` (`MaLoaiSanPham`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_MaHangSanXuatSP_MaHangSanXuatHSX`
     FOREIGN KEY (`MaHangSanXuat`)
-    REFERENCES `QuanLySanPham`.`HangSanXuat` (`MaHangSanXuat`)
+    REFERENCES `eshopv`.`HangSanXuat` (`MaHangSanXuat`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QuanLySanPham`.`ChiTietDonDatHang`
+-- Table `eshopv`.`ChiTietDonDatHang`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`ChiTietDonDatHang` (
+CREATE TABLE IF NOT EXISTS `eshopv`.`ChiTietDonDatHang` (
   `MaChiTietDonDatHang` VARCHAR(15) NOT NULL,
   `SoLuong` INT NULL,
   `GiaBan` INT NULL,
@@ -132,12 +131,12 @@ CREATE TABLE IF NOT EXISTS `QuanLySanPham`.`ChiTietDonDatHang` (
   INDEX `fk_MaSanPhamCTDDH_MaSanPhamSP_idx` (`MaSanPham` ASC), # VISIBLE,
   CONSTRAINT `fk_MaDonDatHangCTDDH_MaDonDatHangDDH`
     FOREIGN KEY (`MaDonDatHang`)
-    REFERENCES `QuanLySanPham`.`DonDatHang` (`MaDonDatHang`)
+    REFERENCES `eshopv`.`DonDatHang` (`MaDonDatHang`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_MaSanPhamCTDDH_MaSanPhamSP`
     FOREIGN KEY (`MaSanPham`)
-    REFERENCES `QuanLySanPham`.`SanPham` (`MaSanPham`)
+    REFERENCES `eshopv`.`SanPham` (`MaSanPham`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;

0eb32320e81456edc1cd245a5f7b9dd0ef21ee6e
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2018-12-10 22:48:21 +0700

    folder eshopvp  thư mục sẽ chạy đồ án (php, kết nối mysql...)

---
+++ e-shopvp/Model/database/config.php
@@ -0,0 +1,19 @@
+<?php
+//cấu hình kết nối cơ sở dữ liệu
+
+//host
+define('DB_HOST', "localhost");
+//user sử dụng
+define('DB_USER', "root");
+//mật khẩu
+define('DB_PASSWORD', "");
+//database sử dụng
+define('DB_DATABASE', "eshopv");
+//port: cổng mạng 
+define('DB_PORT', 3308);
+
+//thông số controller và action mặc định
+define('CONTROLLER_DEFAULT', 'index');
+define('ACTION_DEFAULT', 'index');
+
+?>

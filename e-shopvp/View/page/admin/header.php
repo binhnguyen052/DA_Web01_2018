9331c67907ad126b00f0c4ea568da3f0c5941aad
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2018-12-10 23:42:19 +0700

    Update header.php

--- e-shopvp/View/page/admin/header.php
+++ e-shopvp/View/page/admin/header.php
@@ -38,11 +38,13 @@
   <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
     <a class="navbar-brand mr-1" href="index.php">Start Bootstrap</a>
-
+    
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
     </button>
 
+    <a class="navbar-brand mr-1 text-info" href="../../../index.html">Home</a>
+    
     <!-- Navbar Search -->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
       <div class="input-group">

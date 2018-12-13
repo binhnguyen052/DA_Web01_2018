3ef59aa42b9f48fddc669ece41dcf872dec396b8
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-13 16:21:14 +0700

    update

---
+++ old/e-shopvp/View/page/admin/order.php
@@ -0,0 +1,43 @@
+<?php
+  include("./header.php");
+  ?>
+
+    <div id="wrapper">
+
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.php">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Product Orders</li>
+          </ol>
+
+          <!-- Page Content -->
+          <h1>Quản lý đơn đặt hàng</h1>
+          <hr>
+          <p>This is a great starting point for new custom pages.</p>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <!-- Sticky Footer -->
+        <footer class="sticky-footer">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright © Your Website 2018</span>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+      <!-- /.content-wrapper -->
+
+
+<?php
+    include("./footer.php");
+?>

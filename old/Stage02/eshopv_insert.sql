22cd9753b3acfb21d7c7e627dc2cfaeae6d0afed
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2018-12-15 07:51:35 +0700

    update sql

--- old/Stage02/eshopv_insert.sql
+++ old/Stage02/eshopv_insert.sql
@@ -227,51 +227,6 @@ values
 (6, 0, 4, 12),
 (6, 0, 4, 20);
 
--- procedure cập nhật số hàng đã bán
-drop procedure if exists proc_sold_count
-DELIMITER $$
-create procedure proc_sold_count()
-begin 
-	declare _count int; 
-    declare product_id int;
-    declare no_more_products int default 1;
-    declare cur cursor for select product.id, count(order_detail.id) 
-								from product join order_detail on product.id = order_detail.product_id 
-								group by product.id;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND set no_more_products = 0;
-    open cur;
-    m_loop: loop
-		fetch cur into product_id, _count;
-		if no_more_products = 0 then leave m_loop;
-        end if;
-		UPDATE product SET product.sold = _count WHERE product.id = product_id;	
-	end loop m_loop;
-    close cur;
-end $$
-
-call proc_sold_count();
-
-/* trigger cập nhật thuộc tính sold trên bảng product
- khi thêm 1 dòng chi tiết đơn hàng thì thuộc tính sold tăng lên 1
-bảng tầm ảnh hưởng
-ORDERS: insert +(id)
-ORDER_DETAIL: insert +(id, order_id, product_id)
-PRODUCT: update +(id, + sold)
-
-Từ khóa OLD chỉ đến dòng dữ liệu đang tồn tại trước khi thực hiện thao tác chỉnh sửa. 
-Từ khóa NEW chỉ đến dòng dữ liệu mới xuất hiện sau khi thực hiện thao tác chỉnh sửa.
-*/
-
-drop trigger if exists trigger_sold_update
-DELIMITER $$
-create trigger trigger_sold_update before insert
-on order_detail
-for each row
-begin
-	update product set product.solds = product.solds + 1 where product.id = NEW.product_id;
-end$$
-
-
 
 
 

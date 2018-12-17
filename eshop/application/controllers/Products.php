ba893b86bf0afc51667b8aac18f668952102d4e0
Tieucahcmus <45475099+Tieucahcmus@users.noreply.github.com>
2018-12-17 10:48:26 +0700

    admin

--- eshop/application/controllers/Products.php
+++ eshop/application/controllers/Products.php
@@ -5,15 +5,21 @@ class Products extends CI_Controller
 {
     public function index()
     {
+        $params = $this->input->get();
+
+        //echo '<pre>'; print_r($params); echo '</pre>'; exit();
         $this->load->model('Product');
         $manufacturers = $this->Product->get_manufacturer();
         $product_type = $this->Product->get_product_type();
         $product_type_manufacturers = $this->Product->get_product_type_manufacturer(1);
+        $products = $this->Product->get_all($params);
+        //echo '<pre>'; print_r($products); echo '</pre>'; exit();
 
         $data = array(
             'product_type' => $product_type,
             'manufacturers' => $manufacturers,
-            'product_type_manufacturers' => $product_type_manufacturers
+            'product_type_manufacturers' => $product_type_manufacturers,
+            'products' => $products
         );
 
         $this->load->view('header', $data);

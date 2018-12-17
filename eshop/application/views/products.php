0f5f6f58adfc5b1b9a98b71de6dba05b02e12f6f
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-17 13:43:20 +0700

    name

--- eshop/application/views/products.php
+++ eshop/application/views/products.php
@@ -214,7 +214,7 @@
                                         <i class="fa fa-star"></i>
                                         <i class="fa fa-star-o empty"></i>
                                     </div>
-                                    <h2 class="product-name"><a href="#">Tên Sản Phẩm</a></h2>
+                                    <h2 class="product-name text-uppercase"><a href="#"><?php echo $_product['name'];?></a></h2>
                                     <div class="product-btns">
                                         <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
                                         <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>

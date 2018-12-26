3fe9a2cf62b54cbbe0c4fb21effe372d44c6806b
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-26 10:15:29 +0700

    Update product-page.php

--- e-shopvp/views/page/product-page.php
+++ e-shopvp/views/page/product-page.php
@@ -49,10 +49,10 @@
 					<div class="col-md-6">
 						<div class="product-body">
 							<div class="product-label">
-								<span>Sản Phẩm Mới</span>
-								<span class="sale">-20%</span>
+								<span class="text-uppercase">Sản Phẩm</span>
+<!--								<span class="sale">-20%</span>-->
 							</div>
-							<h2 class="product-name">Adidas Superstar</h2>
+							<h2 class="product-name text-uppercase">Adidas Superstar</h2>
 							<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
 							<div>
 								<div class="product-rating">

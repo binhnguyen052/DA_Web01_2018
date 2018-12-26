200efc7c5900e761cacf1b11c94c537264746774
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-26 11:46:28 +0700

    Update product-page.php

--- e-shopvp/views/page/product-page.php
+++ e-shopvp/views/page/product-page.php
@@ -143,14 +143,14 @@
 						</div>
 						<div class="product-body">
 							<h3 class="product-price">$32.50</h3>
-							<div class="product-rating">
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star-o empty"></i>
-							</div>
-							<h2 class="product-name"><a href="#">Tên sản phẩm</a></h2>
+<!--							<div class="product-rating">-->
+<!--								<i class="fa fa-star"></i>-->
+<!--								<i class="fa fa-star"></i>-->
+<!--								<i class="fa fa-star"></i>-->
+<!--								<i class="fa fa-star"></i>-->
+<!--								<i class="fa fa-star-o empty"></i>-->
+<!--							</div>-->
+							<h2 class="product-name text-uppercase href="#"><?php echo $row_type['name'];?></a></h2>
 							<div class="product-btns">
 								<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
 								<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>

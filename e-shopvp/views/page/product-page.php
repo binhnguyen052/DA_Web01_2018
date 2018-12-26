28fd87dbda40ec417f21ee176adf49843a1ffa49
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-26 11:31:06 +0700

    Update product-page.php

--- e-shopvp/views/page/product-page.php
+++ e-shopvp/views/page/product-page.php
@@ -66,8 +66,7 @@
                                 <p class="text-uppercase"><strong>Loại sản phẩm: </strong><?php echo $row['type_name']; ?></p>
                                 <p class="text-uppercase"><strong>Nhà sản xuất: </strong><?php echo $row['manufacturer_name']; ?></p>
                                 <p class="text-uppercase"><strong>Xuất xứ: </strong><?php echo $row['origin']; ?></p>
-                                <p class="text-uppercase">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <p class="text-uppercase"><strong>Mô tả: </strong><?php echo $row['descreibe']; ?></p>
                                 <div class="product-options">
                                     <ul class="size-option">
                                         <li><span class="text-uppercase">Size:</span></li>

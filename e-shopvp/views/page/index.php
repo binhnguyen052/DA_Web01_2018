7dae5e3af006e0ec6f6542f87da264f135f5a021
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-26 07:21:39 +0700

    Update index.php

--- e-shopvp/views/page/index.php
+++ e-shopvp/views/page/index.php
@@ -252,7 +252,7 @@ $href_public = '../../public';
                                             <i class="fa fa-star"></i>
                                             <i class="fa fa-star-o empty"></i>
                                         </div>
-                                        <h2 class="product-name"><a href="#">Xem chi tiết</a></h2>
+                                        <h2 class="product-name text-uppercase"><a href="#"><?php echo $row['name']; ?></a></h2>
                                         <div class="product-btns">
                                             <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
                                             <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>

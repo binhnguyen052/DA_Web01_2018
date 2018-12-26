c59b7a4f59860df9b29d21d0294c72c0fe47d270
haonguyen1660164 <45476657+haonguyen1660164@users.noreply.github.com>
2018-12-26 11:36:17 +0700

    update product-detail e-shopvp

--- e-shopvp/views/page/product-page.php
+++ e-shopvp/views/page/product-page.php
@@ -125,12 +125,19 @@
 				</div>
 				<!-- section title -->
 
+                <?php
+                    $sql = $_model_product->get_same_type($row['id']);
+                    $result = $db->executeQuery($db->link, $sql);
+                    while($row = mysqli_fetch_array($result))
+                    {
+                    extract($row);
+                ?>
 				<!-- Product Single -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
+				<div class="col-md-2 col-sm-6 col-xs-6">
 					<div class="product product-single">
 						<div class="product-thumb">
 							<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem Nhanh</button>
-							<img src="../img/product04.jpg" alt="">
+							<img src=".<?php echo $href_public?>/upload/<?php echo $row['image_url'];?>product04.jpg" alt="">
 						</div>
 						<div class="product-body">
 							<h3 class="product-price">$32.50</h3>
@@ -151,127 +158,7 @@
 					</div>
 				</div>
 				<!-- /Product Single -->
-
-				<!-- Product Single -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="product product-single">
-						<div class="product-thumb">
-							<div class="product-label">
-								<span>Hàng Mới</span>
-							</div>
-							<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem Nhanh</button>
-							<img src="../img/product03.jpg" alt="">
-						</div>
-						<div class="product-body">
-							<h3 class="product-price">$32.50</h3>
-							<div class="product-rating">
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star-o empty"></i>
-							</div>
-							<h2 class="product-name"><a href="#">Tên Sản Phẩm</a></h2>
-							<div class="product-btns">
-								<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-								<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-								<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i>  Thêm vào giỏ hàng</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- /Product Single -->
-
-				<!-- Product Single -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="product product-single">
-						<div class="product-thumb">
-							<div class="product-label">
-								<span class="sale">-20%</span>
-							</div>
-							<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem Nhanh</button>
-							<img src="../img/product02.jpg" alt="">
-						</div>
-						<div class="product-body">
-							<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
-							<div class="product-rating">
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star-o empty"></i>
-							</div>
-							<h2 class="product-name"><a href="#">Tên Sản Phẩm</a></h2>
-							<div class="product-btns">
-								<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-								<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-								<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i>  Thêm Vào Giỏ Hàng</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- /Product Single -->
-
-				<!-- Product Single -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="product product-single">
-						<div class="product-thumb">
-							<div class="product-label">
-								<span>Hàng Mới</span>
-								<span class="sale">-20%</span>
-							</div>
-							<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem Nhanh</button>
-							<img src="../img/product01.jpg" alt="">
-						</div>
-						<div class="product-body">
-							<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
-							<div class="product-rating">
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-							<i class="fa fa-star-o empty"></i>
-							</div>
-							<h2 class="product-name"><a href="#">tên Sản Phẩm</a></h2>
-							<div class="product-btns">
-								<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-								<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-								<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i>  Thêm Vào Giỏ Hàng</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- /Product Single -->
-
-				<!-- Product Single -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="product product-single">
-						<div class="product-thumb">
-							<div class="product-label">
-								<span>Hàng Mới</span>
-								<span class="sale">-20%</span>
-							</div>
-							<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem Nhanh</button>
-							<img src="../img/product01.jpg" alt="">
-						</div>
-						<div class="product-body">
-							<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
-							<div class="product-rating">
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star-o empty"></i>
-							</div>
-							<h2 class="product-name"><a href="#">tên Sản Phẩm</a></h2>
-							<div class="product-btns">
-								<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-								<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-								<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i>  Thêm Vào Giỏ Hàng</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- /Product Single -->	
+                <?php } ?>
 
 			</div>
 			<!-- /row -->
@@ -289,7 +176,7 @@
 				<!-- section title -->
 				<div class="col-md-12">
 					<div class="section-title">
-						<h2 class="title">sản phẩm cùng nhà sản xuất</h2>
+						<h2 class="title">cùng nhà sản xuất</h2>
 					</div>
 				</div>
 				<!-- section title -->
